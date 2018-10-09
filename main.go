36626fced30bd673886c1e3b4637f6b8abf46d41
Jon Gunnar Fossum <jongfo@stud.ntnu.no>
2018-10-09 09:47:33 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/marni/goigc"
+)
+
+func main() {
+	s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
+	track, err := igc.ParseLocation(s)
+	if err != nil {
+		fmt.Errorf("Problem reading the track", err)
+	}
+
+	fmt.Printf("Pilot: %s, gliderType: %s, date: %s",
+		track.Pilot, track.GliderType, track.Date.String())
+}
