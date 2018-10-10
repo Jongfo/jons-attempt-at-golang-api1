04f9cac1242f2f0d48e6bdcad04f5d114379b72d
Jon Gunnar Fossum <jongfo@stud.ntnu.no>
2018-10-10 15:23:16 +0200

    Mariusz code uncomment

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	igc "github.com/marni/goigc"
 )
 
+//test func that helped me understand task better
 func hello(w http.ResponseWriter, r *http.Request) {
 	//io.WriteString(w, "Hello World")
 	fmt.Fprint(w, "Hello Person!")
@@ -15,14 +18,14 @@ func main() {
 	port := os.Getenv("PORT")
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":"+port, nil)
-	/*
-		s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
-		track, err := igc.ParseLocation(s)
-		if err != nil {
-			fmt.Errorf("Problem reading the track", err)
-		}
 
-		fmt.Printf("Pilot: %s, gliderType: %s, date: %s",
-			track.Pilot, track.GliderType, track.Date.String())
-	*/
+	s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
+	track, err := igc.ParseLocation(s)
+	if err != nil {
+		fmt.Errorf("Problem reading the track", err)
+	}
+
+	fmt.Printf("Pilot: %s, gliderType: %s, date: %s",
+		track.Pilot, track.GliderType, track.Date.String())
+
 }
