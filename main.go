41a5b60dcdb49fc56e28f460ac1b172dac81ae3c
Jon Gunnar Fossum <jongfo@stud.ntnu.no>
2018-10-10 14:55:45 +0200

    hello world

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/marni/goigc"
+	"io"
+	"net/http"
+	"os"
 )
 
-func main() {
-	s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
-	track, err := igc.ParseLocation(s)
-	if err != nil {
-		fmt.Errorf("Problem reading the track", err)
-	}
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello World")
+}
 
-	fmt.Printf("Pilot: %s, gliderType: %s, date: %s",
-		track.Pilot, track.GliderType, track.Date.String())
-	//fmt.Println("Hello world!")
+func main() {
+	port := os.Getenv("PORT")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":"+port, nil)
+	/*
+		s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
+		track, err := igc.ParseLocation(s)
+		if err != nil {
+			fmt.Errorf("Problem reading the track", err)
+		}
+
+		fmt.Printf("Pilot: %s, gliderType: %s, date: %s",
+			track.Pilot, track.GliderType, track.Date.String())
+	*/
 }
